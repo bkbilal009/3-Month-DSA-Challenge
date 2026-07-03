049381a1a7edfe8f6164c21898f778d1689d8b9e
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-03 14:47:07 +0500

    Create patterns.cpp

---
+++ Month-01/Week-01/Day-02/patterns.cpp
@@ -0,0 +1,183 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int n;
+//     cin >> n;
+    
+//     int i = 1;
+//     while (i<=n){
+//         int j = 1;
+//         while (j <= n){
+//             cout << "*";
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+
+
+//     return 0;
+// }
+
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+
+//     while (i <= n){
+//         int j = 1;
+//         while (j<=n){
+//             cout << i;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+
+//     while (i<=n){
+//         int j = 1;
+//         while (j <= n){
+//             cout << j;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+
+//     while (i<=n){
+//         int j = 1;
+//         while (j <= n){
+//             cout << n-j+1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+
+//     return 0;
+// }
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int i = 1;
+//     int count = 1;
+//     while (i <=n){
+//         int j = 1;
+//         while (j <= n){
+//             cout << count<< " ";
+//             count = count + 1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+
+//     return 0;
+// }
+
+
+// int main(){
+
+//     int n ;
+//     cin >> n;
+
+//     int row = 1;
+
+//     while(row <= n){
+//         int col = 1;
+//         while(col <= row){
+//             cout << "*";
+//             col = col + 1;
+//         }
+//         cout << endl;
+//         row = row + 1;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+// int main(){
+//     int n;
+//     cin >> n;
+
+//     int row = 1;
+//     while (row <= n){
+//         int col = 1;
+
+//         while (col <= row){
+//             cout << row;
+//             col = col + 1;
+//         }
+//         cout << endl;
+//         row = row + 1;
+//     }
+
+
+//     return 0;
+// }
+
+
+
+// 5
+// 1
+// 23
+// 345
+// 4567
+// 56789
+
+// int main(){
+
+//     int n;
+//     cin >> n;
+
+//     int row = 1;
+//     while (row <= n){
+//         int col = 1;
+//         int val = row;
+//         while (col <= row){
+//             cout << val;
+//             val = val + 1;
+//             col = col + 1;
+//         }
+//         cout << endl;
+//         row = row + 1;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+
