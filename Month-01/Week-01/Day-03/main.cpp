423a18aed0c92186fe824e2938a68def045e9473
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-11 12:29:30 +0500

    Create main.cpp

---
+++ Month-01/Week-01/Day-03/main.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+
+//     char ch;
+//     cout << "Enter your value"<<endl;
+//     cin >> ch;
+
+//     if (ch >= 'a' && ch <= 'z'){
+//         cout << "This is lowercase"<<endl;
+//     }
+//     else if (ch >= 'A' && ch <= 'Z')
+//     {
+//         cout << "This is uppercase" << endl;
+//     }
+//     else if (ch >= '1' && ch <= '9'){
+//         cout << "This is numeric" << endl;
+//     }
+//     else 
+//     {
+//         cout << "This is a special character " << endl;
+//     }
+
+//     return 0;
+// }
+
+
+
+
+
+// int main(){
+
+//     int n;
+//     cout << "Enter your value" << endl;
+//     cin >> n;
+
+//     if (n%2 == 0){
+//         cout << "Even no:";
+//     }
+//     else if (n%2 != 0){
+//         cout << "Odd no:";
+//     }
+//     else
+//     {
+//         cout << "Simple no";
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+// int main(){
+
+//     int num ;
+//     cin >> num;
+
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         /* code */
+//         cout << num << "x" << i << "=" << num*i << endl;
+//     }
+    
+
+//     return 0;
+// }
