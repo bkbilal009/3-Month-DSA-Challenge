d109cd89065198841294ced3b4b850dd39dcbb66
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-12 10:15:17 +0500

    Create Post & Pre increment.cpp

---
+++ Month-01/Week-01/Day-03/Post & Pre increment.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // +++++++++++++++++++++++++++++++++++++++++ Post & Pre increment +++++++++++++++++++++++++++++++++++++++++++++
+
+    int i = 7;
+
+    cout << (++i) << endl; // 8
+    // 1 + 8 = 8 , i = 8
+    cout << (i++) << endl; // 8
+    // 8 + 1 = 9 , i = 9
+    cout << (i--) << endl; // 9
+    // 9 - 1 = 8 , i = 8
+    cout << (--i) << endl; // 7
+    // 1 - 8 = 7 , i = 7
+
+    return 0;
+}
+
+
