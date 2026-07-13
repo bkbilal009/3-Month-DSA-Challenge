39a91e71b33c1c8bbea9851be07288be4bd0e594
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-13 09:11:49 +0500

    Create assignment five question.cpp

---
+++ Month-01/Week-01/Day-03/assignment five question.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Question no 1:
+
+    // int a = 1;
+    // int b = 2;
+
+    // if ((a-- > 0) && (++b > 2)){
+    //     cout << "Stage 1 -Inside If ";
+    // }
+    // else{
+    //     cout << "Stage 2 -Inside else";
+    // }
+    // cout << a << " " << b << endl;
+
+    // Question no 2:
+
+    // int a = 1;
+    // int b = 2;
+
+    // if ((a-- > 0) || (++b > 2)){
+    //     cout << "Stage 1 -Inside If ";
+    // }
+    // else{
+    //     cout << "Stage 2 -Inside else";
+    // }
+    // cout << a << " " << b << endl;
+
+
+    // Question no 3:
+
+    // int num = 3;
+    // cout << (25 * (++num));
+
+
+
+    // Question no 4:
+
+    
+//     int a = 1;
+//     int b = a++;
+//     int c = ++a;
+//     cout << c;
+
+//     return 0;
+// }
