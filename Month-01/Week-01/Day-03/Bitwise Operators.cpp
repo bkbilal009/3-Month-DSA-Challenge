53773ae8b4daa47bac02ff83f556675959f359c5
Muhammad Bilal <bkbilalbluch@gmail.com>
2026-07-16 08:40:08 +0500

    Update Bitwise Operators.cpp

--- Month-01/Week-01/Day-03/Bitwise Operators.cpp
+++ Month-01/Week-01/Day-03/Bitwise Operators.cpp
@@ -18,5 +18,26 @@ int main(){
     cout << (19 << 1) << endl;
     cout << (21 << 2) << endl;
 
+
+
+    int n = 10;
+    int a = 0;
+    int b = 1;
+
+    cout << a << " " << b << " ";
+
+    for (int i = 1; i <= n; i++){
+        int nextNumber = a + b;
+        cout << nextNumber << " ";
+        
+        a = b;
+        b = nextNumber;
+    }
+
+
+
+
+    
+
     return 0;
 }
